ca56ec5897ecc348ae33175ee48f708f010b2041
clut <56116386+2233wwqq@users.noreply.github.com>
2025-12-06 03:27:23 +0900

    Main

---
+++ src/main/java/knu/database/musicbase/MusicBaseApplication.java
@@ -0,0 +1,13 @@
+package knu.database.musicbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicBaseApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MusicBaseApplication.class, args);
+    }
+}
